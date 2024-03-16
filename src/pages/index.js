767b2fc9b06124cfc39762225c10ceab6d52b834
Samuel Rodriguez <samuelyret@gmail.com>
2024-03-16 19:26:08 +0100

    Add basic vertical menu.

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,29 @@ const IndexPage = () => (
       alt=""
       style={{ marginBottom: `var(--space-3)` }}
     /> */}
+    <div
+      id="menu1"
+      class="flex justify-start  flex-col w-full md:w-auto items-start pb-1 "
+    >
+      <button class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+        <p class="text-base leading-4  ">Intro</p>
+      </button>
+      <button class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+        <p class="text-base leading-4  ">Work experience</p>
+      </button>
+      <button class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+        <p class="text-base leading-4  ">Skills</p>
+      </button>
+      <button class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+        <p class="text-base leading-4  ">Project</p>
+      </button>
+      <button class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+        <p class="text-base leading-4  ">About</p>
+      </button>
+      <button class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
+        <p class="text-base leading-4  ">Contact</p>
+      </button>
+    </div>
   </Layout>
 )
 
